573fafc063c4385240a09d39e5a14a8c4f76ee85
DragonCeura <tai.drahcir@gmail.com>
2018-04-27 00:05:43 -0700

    feat: Integrate BooksAPI and use React Router
    Update page navigation/screen showing to be handled by React Router.
    
    Query the BooksAPI for full set of books to be shelved.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -7,23 +8,33 @@ import ListBooks from './components/ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books: books
+        }))
+      })
+  };
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks />
-        ) : (
-          <ListBooks />
-        )}
+        <Route exact path ='/' render={() => (
+          <ListBooks
+            books={this.state.books}
+          />
+        )} />
+        <Route exact path ='/search' render={({ history }) => (
+          <SearchBooks
+            onSearchBooks={(book) => {
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     )
   }
