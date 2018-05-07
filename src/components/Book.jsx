0badc099b9a9e5df7bfafbf2095735791dd9e2f4
DragonCeura <tai.drahcir@gmail.com>
2018-05-06 23:13:50 -0700

    bug: Remove props usage in stateless functional component Book
    Finish refactor of stateless functional components to use destructured props as arguments.

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import { bookshelves } from './../utils/Utils'
 
 function Book ({ book, shelfChanger }) {
-  const imageUrl = props.book.imageLinks ? props.book.imageLinks.thumbnail : 'https://books.google.com/googlebooks/images/no_cover_thumb.gif'
+  const imageUrl = book.imageLinks ? book.imageLinks.thumbnail : 'https://books.google.com/googlebooks/images/no_cover_thumb.gif'
   return (
     <li>
       <div className="book">
@@ -14,8 +14,8 @@ function Book ({ book, shelfChanger }) {
             style={{ width: 128, height: 193, backgroundImage: `url(${imageUrl})` }}
           />
           <div className="book-shelf-changer">
-            <select onChange={(event) => props.shelfChanger(props.book, event.target.value)}
-              value={props.book.shelf}>
+            <select onChange={(event) => shelfChanger(book, event.target.value)}
+              value={book.shelf}>
               <option value="moveTo" disabled>Move to...</option>
               {bookshelves.map((bookshelf) => (
                 <option key={bookshelf.value} value={bookshelf.value}>{bookshelf.header}</option>
@@ -24,8 +24,8 @@ function Book ({ book, shelfChanger }) {
             </select>
           </div>
         </div>
-        <div className="book-title">{props.book.title}</div>
-        <div className="book-authors">{props.book.authors && props.book.authors.join(", ")}</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors && book.authors.join(", ")}</div>
       </div>
     </li>
   )
